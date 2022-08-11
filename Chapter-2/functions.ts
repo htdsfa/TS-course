cd37085a3828f48901c12f12e95e12c713e8d32a
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 01:53:41 +0200

    Update functions.ts

--- Chapter-2/functions.ts
+++ Chapter-2/functions.ts
@@ -6,6 +6,12 @@ function printResult(num: number): void {
     console.log('Result: ' + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void {
+    const result = n1 + n2;
+    cb(result);
+
+}
+
 printResult(add(5, 12))
 //let someValue: undefined;
 let combineValues: (a: number, b: number) => number;
@@ -14,3 +20,6 @@ combineValues = add;
 //combineValues = printResult;
 //combineValues =5;
 console.log(combineValues)(8, 8);
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+})
