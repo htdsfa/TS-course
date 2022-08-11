6c189e0bdff54e4e90a9e578f220f485b395e80f
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 00:17:42 +0200

    Update Objects.ts (Enums)

--- Chapter-2/Objects.ts
+++ Chapter-2/Objects.ts
@@ -2,17 +2,31 @@
 //    name: string
 //    age: number
 //} ={
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string]
-} = {
+//const person: {
+//    name: string
+//    age: number
+//    hobbies: string[]
+//    role: [number, string]
+//} = {
+//    name: 'Wojtek',
+//    age: 18,
+//    hobbies: ["Sports", "Cooking"],
+//    role: [2, "author"]
+//};
+
+//const ADMIN = 0;
+//const READ_ONLY = 1;
+//const AUTHOR = 2;
+
+enum Role {ADMIN = 5, READ_ONLY = 'read', AUTHOR = 15}
+
+const person = {
     name: 'Wojtek',
     age: 18,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: Role.ADMIN
 };
+
 
 //person.role.push('admin');
 //person.role[1] = 10;
@@ -26,4 +40,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     //console.log(hobby.map())
+}
+
+if (person.role === Role.ADMIN) {
+    console.log('is admin')
 }
