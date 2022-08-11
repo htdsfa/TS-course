df82c917c8f6ac71a4e5b0cf2d738b9bd6235526
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-11 23:55:00 +0200

    Update Objects.ts

--- Chapter-2/Objects.ts
+++ Chapter-2/Objects.ts
@@ -1,10 +1,19 @@
-const person: {
-    name: string
-    age: number
-} ={
-    //const person={
+//const person: {
+//    name: string
+//    age: number
+//} ={
+const person = {
     name: 'Wojtek',
-    age: 18
+    age: 18,
+    hobbies: ["Sports", "Cooking"]
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    //console.log(hobby.map())
+}
