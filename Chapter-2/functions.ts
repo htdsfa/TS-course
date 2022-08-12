acf349966d764740ca679a1dd8562e6b7d742511
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 05:12:41 +0200

    Update functions.ts

--- Chapter-2/functions.ts
+++ Chapter-2/functions.ts
@@ -9,7 +9,6 @@ function printResult(num: number): void {
 function addAndHandle(n1: number, n2: number, cb: (num: number) => void {
     const result = n1 + n2;
     cb(result);
-
 }
 
 printResult(add(5, 12))
