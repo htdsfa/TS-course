d57e3a9b068c3f51af20c288a531c05cc98aa1d2
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 09:47:50 +0200

    Update Interfaces.ts

--- Chapter-5/Interfaces.ts
+++ Chapter-5/Interfaces.ts
@@ -1,16 +1,24 @@
-interface Person {
+interface Greetable {
     name: string;
-    age: number;
 
     greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+    name: string;
+    age: 19;
 
-user1 = {
-    name: 'Max', age: 19, greet(phrase: string) {
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet(phrase: string) {
         console.log(phrase + '' + this.name);
     }
-};
+}
 
+let user1: Greetable;
+
+user1 = Person('Max')
 user1.greet('hello')
+console.log(user1)
