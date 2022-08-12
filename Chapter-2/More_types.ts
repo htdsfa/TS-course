c3962fa3919d1faf1baaeb3b6350af75514b8336
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 03:11:53 +0200

    Update More_types.ts

--- Chapter-2/More_types.ts
+++ Chapter-2/More_types.ts
@@ -5,3 +5,9 @@ userInput='Max'
 if (typeof userInput==='string') {
     userName = userInput
 }
+
+function generateError(message:string,code:number):never{
+    throw {message:message, error:code}
+    //while (true){}
+}
+generateError('An Error has occurred', 500)
