aa94ebad7ba271b3d600000b26c586b0a15a07c4
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-12 08:18:07 +0200

    Update Class.ts

--- Chapter-5/Class.ts
+++ Chapter-5/Class.ts
@@ -1,7 +1,7 @@
 class Department {
     //private id: string;
     //private name: string;
-    private employees: string[] = []
+    protected employees: string[] = []
 
     constructor(private readonly id: string, public name: string) {
         //this.id = id
@@ -38,6 +38,13 @@ class AccountingDepartment extends Department {
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
     }
+    addEmployee(name: string) {
+        if(name==='Max'){
+            return;
+
+        }
+        this.employees.push(name)
+    }
 
     addReport(text: string) {
         this.reports.push(text)
@@ -53,12 +60,20 @@ const it = new ITDepartment('d1', ['Accounting']);
 
 it.addEmployee('Max')
 it.addEmployee('Manuel')
+
 //accounting.employees[2] = 'anna';
 it.describe();
+
 it.printEmployeeInformation();
+
 console.log(it)
+
 const accounting = new AccountingDepartment('d2', [])
 accounting.addReport('Something went wrong')
+
+accounting.addEmployee('Max')
+accounting.addEmployee('Manuel')
+
 accounting.printReports();
 //const accountingCopy = {name: 's', describe: accounting.describe}
 //accountingCopy.describe()
