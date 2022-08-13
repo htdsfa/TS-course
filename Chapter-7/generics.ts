56ae2af93fcd46abf069e842ba94cf8db395fcc3
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 03:30:56 +0200

    Update generics.ts

--- Chapter-7/generics.ts
+++ Chapter-7/generics.ts
@@ -8,3 +8,16 @@ const promise: Promise<number> = new Promise((resolve, reject) => {
 promise.then(data => {
     //data.split('');
 })
+
+//***********************************************************************************
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge<{ name: string, hobbies: string[] }, { age: number, }>({name: 'Max', hobbies: ['Sports']}
+{
+    age: 30
+}
+)
+;
+console.log(mergedObj.age)
