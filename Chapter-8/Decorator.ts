1bb363b7df826a85b650796ec1f6b3ec9c9ba160
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 06:16:14 +0200

    Update Decorator.ts

--- Chapter-8/Decorator.ts
+++ Chapter-8/Decorator.ts
@@ -97,3 +97,30 @@ class Product {
 
     }
 }
+
+function AutoBind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn
+        }
+
+    }
+    return adjDescriptor
+}
+
+
+class Printer {
+    message = 'This works'
+@AutoBind
+    showMessage() {
+        console.log(this.message)
+    }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage)
