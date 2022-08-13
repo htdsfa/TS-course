757e15bd0cdbe1fab7920ac8ebe11dbd24ed8a74
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 02:09:09 +0200

    Update Intersection_types.ts (Discriminated Unions)

--- Chapter-6/Intersection_types.ts
+++ Chapter-6/Intersection_types.ts
@@ -43,6 +43,7 @@ function printEmployeeInformation(emp: UnknownEmployee) {
 
 printEmployeeInformation({name: 'Manu', startDate: new Date()})
 
+//***********************************************************************************
 class Car {
     drive() {
         console.log('Driving')
@@ -75,3 +76,33 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1)
 useVehicle(v2)
+
+//***********************************************************************************
+interface Bird {
+    type: 'bird';
+    flyingSpeed: number;
+}
+
+interface Horse {
+    type: 'horse';
+    runningSpeed;
+    number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.flyingSpeed
+            break;
+        case 'horse':
+            speed = animal.runningSpeed
+            break;
+    }
+    console.log('Moving at speed: ' + speed)
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10})
+//***********************************************************************************
