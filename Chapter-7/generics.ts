fb7a19046d05fd54dceda719854044d248d4c8cf
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 03:34:55 +0200

    Update generics.ts

--- Chapter-7/generics.ts
+++ Chapter-7/generics.ts
@@ -10,14 +10,9 @@ promise.then(data => {
 })
 
 //***********************************************************************************
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string, hobbies: string[] }, { age: number, }>({name: 'Max', hobbies: ['Sports']}
-{
-    age: 30
-}
-)
-;
-console.log(mergedObj.age)
+const mergedObj = merge<{ name: string, hobbies: string[] }, { age: number, }>({name: 'Max', hobbies: ['Sports']},{age: 30});
+console.log(mergedObj)
