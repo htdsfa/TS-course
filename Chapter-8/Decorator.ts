d409d6177a2f17ca587e50c43314b4e7808d6d71
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 05:36:25 +0200

    Update Decorator.ts

--- Chapter-8/Decorator.ts
+++ Chapter-8/Decorator.ts
@@ -7,6 +7,7 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
     return function (constructor: any) {
+        console.log('Rendering template')
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -17,6 +18,7 @@ function WithTemplate(template: string, hookId: string) {
 }
 
 //@Logger('LOGGING - PERSON')
+@Logger('LOGGING')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max'
@@ -29,3 +31,36 @@ class Person {
 
 const pers = new Person();
 console.log(pers)
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator')
+    console.log(target, propertyName)
+
+}
+
+class Product {
+    @Log
+    title: string
+    private _price: number
+
+    ser
+
+    price(val: number) {
+        if (val > 0) {
+            this._price = val
+        } else {
+            throw new Error('Invalid price')
+        }
+
+    }
+
+    constructor(t: string) {
+        this.title = t
+        this._price = p
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+
+    }
+}
