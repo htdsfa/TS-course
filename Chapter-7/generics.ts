1b5a2462afb3a7c9db495ef537def7e5de4c54e6
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 03:44:13 +0200

    Update generics.ts

--- Chapter-7/generics.ts
+++ Chapter-7/generics.ts
@@ -14,5 +14,26 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string, hobbies: string[] }, { age: number, }>({name: 'Max', hobbies: ['Sports']},{age: 30});
+const mergedObj = merge<{ name: string, hobbies: string[] }, { age: number, }>({
+    name: 'Max',
+    hobbies: ['Sports']
+}, {age: 30});
 console.log(mergedObj)
+
+interface Lengthy {
+    lenght: number;
+
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [t, string] {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 elements';
+    } else if (element.length > 1) {
+        descriptionText = 'Got' + element.length + 'elements.'
+
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there'))
