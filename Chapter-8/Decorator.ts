5db9acd0e0a8e078895a14c7c6e313d3378c13f5
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 05:44:49 +0200

    Update Decorator.ts

--- Chapter-8/Decorator.ts
+++ Chapter-8/Decorator.ts
@@ -38,6 +38,31 @@ function Log(target: any, propertyName: string | Symbol) {
 
 }
 
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+
+    console.log('Accessor decorator')
+    console.log(target)
+    console.log(name)
+    console.log(descriptor)
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator')
+    console.log(target)
+    console.log(name)
+    console.log(descriptor)
+
+}
+
+function Log4(target: any, name: string | Symbol, position: number,) {
+    console.log('Parameter decorator')
+    console.log(target)
+    console.log(name)
+    console.log(position)
+
+}
+
 class Product {
     @Log
     title: string
@@ -59,7 +84,8 @@ class Product {
         this._price = p
     }
 
-    getPriceWithTax(tax: number) {
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
         return this._price * (1 + tax);
 
     }
