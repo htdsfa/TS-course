799ba872b98096892344dbf693b835b93b51cbb2
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-08-13 03:48:57 +0200

    Update generics.ts

--- Chapter-7/generics.ts
+++ Chapter-7/generics.ts
@@ -37,3 +37,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [t, string] {
 }
 
 console.log(countAndDescribe('Hi there'))
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value:' + obj[key]
+}
+
+extractAndConvert(name:'max'}, 'name')
